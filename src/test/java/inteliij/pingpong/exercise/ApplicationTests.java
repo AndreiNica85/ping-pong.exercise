387b278d204eb0ba9f679b91f66950e91ec2df39
Andrei.Nica <andreinica85@yahoo.com>
2022-10-19 16:08:01 +0200

    Commited // message

--- src/test/java/inteliij/pingpong/exercise/ApplicationTests.java
+++ src/test/java/inteliij/pingpong/exercise/ApplicationTests.java
@@ -10,11 +10,13 @@ import java.time.LocalDate;
 @SpringBootTest
 class ApplicationTests {
 
+	 // gery
 	@Test
 	void contextLoads() {
 		Note note = new Note(1, "Do", "some text in here", LocalDate.now(), LocalDate.now(), LocalDate.now(), "http://localhost:8080/notes", 1);
 
 		Assert.isNull(null, note.getDescr());
 	}
+
 
 }
