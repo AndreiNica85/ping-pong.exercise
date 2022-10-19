7cee1d5e2d2ce69638aaa54182d518c44f8d6d37
Piotr Kubacki <piotr.kubacki@solera.com>
2022-10-19 22:53:35 +0200

    added delete note by id

--- src/main/java/inteliij/pingpong/exercise/resource/NoteController.java
+++ src/main/java/inteliij/pingpong/exercise/resource/NoteController.java
@@ -30,10 +30,31 @@ public class NoteController {
             return searchResult.get();
         }
     }
+
+    @DeleteMapping ("/notes/{id}")
+    public Note deleteNote(@PathVariable Integer id){
+        Optional<Note> searchResult =  ns.findNoteById(id);
+        if (searchResult.isEmpty()) {
+            throw new NoteNotFoundException("Note not found!");
+        }
+        else{
+            return ns.deleteNote(searchResult.get());
+        }
+    }
     @PostMapping("/notes")
     public void createNote(@RequestBody Note note){
         ns.addNote(note);
     }
 
+    @PutMapping("/notes/{id}")
+    public Note updateNote(@PathVariable Integer id){
+        Optional<Note> searchResult =  ns.findNoteById(id);
+        if (searchResult.isEmpty()) {
+            throw new NoteNotFoundException("Note not found!");
+        }
+        else{
+            return ns.updateNote(searchResult.get());
+        }
+    }
 
 }
