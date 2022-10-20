b4089a09194f864d130af20c7f61b1097a1438fc
Andrei.Nica <andreinica85@yahoo.com>
2022-10-20 21:18:39 +0200

    Added typeOfLink test case

--- src/test/java/inteliij/pingpong/exercise/ApplicationTests.java
+++ src/test/java/inteliij/pingpong/exercise/ApplicationTests.java
@@ -6,8 +6,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.Assert;
 
 import java.net.MalformedURLException;
-import java.net.URL;
 import java.time.LocalDate;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 @SpringBootTest
 class ApplicationTests {
@@ -26,5 +27,18 @@ class ApplicationTests {
 		int t = note.getText().length();
 		boolean v = t > 10;
 		Assert.isTrue(v,"The string length need to be greater then 10");
+	}
+
+	@Test
+	void testTypeOfLink() throws MalformedURLException {
+		Note noteWeb = new Note(1, "Do", "some text in here", LocalDate.now(), LocalDate.now(), LocalDate.now(),"http://localhost:8080/notes", 1);
+		Note noteEmail = new Note(1, "Do", "some text in here", LocalDate.now(), LocalDate.now(), LocalDate.now(),"http://gertyu@yahoo.com", 1);
+		Note noteYoutube = new Note(1, "Do", "some text in here", LocalDate.now(), LocalDate.now(), LocalDate.now(),"http://youtube.com", 1);
+		String webLink = noteWeb.getLink().toString();
+		String emailLink = noteEmail.getLink().toString();
+		String youtubeLink = noteYoutube.getLink().toString();
+		assertThat(webLink, webLink.contains("localhost"));
+		assertThat(emailLink, emailLink.contains("@"));
+		assertThat(youtubeLink, youtubeLink.contains("youtube"));
 	}
 }
